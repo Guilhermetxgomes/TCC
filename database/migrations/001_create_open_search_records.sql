76c69f2d67a4344952b14716ce39a1e64fc07115
Guilherme Gomes <guilhermetexgomes@gmail.com>
2026-08-22 15:55:12 -0300

    refactor: renaming record to open_search following archtecture 3.0

--- database/migrations/001_create_open_search_records.sql
+++ database/migrations/001_create_open_search_records.sql
@@ -1,9 +1,10 @@
-CREATE TABLE records (
+CREATE TABLE open_search_records (
     record_id SERIAL PRIMARY KEY,
     camera_id INTEGER NOT NULL,
     c_prime TEXT NOT NULL,
     n INTEGER NOT NULL,
     nonce TEXT NOT NULL,
     ciphertext TEXT NOT NULL,
+    captured_at TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
